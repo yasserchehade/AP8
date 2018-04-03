73eb368aedba332b4d21d43a861fc3e56d478865
yasserchehade <36651977+yasserchehade@users.noreply.github.com>
2018-04-03 17:44:54 -0500

    Update AP8.m

--- AP8.m
+++ AP8.m
@@ -3,45 +3,49 @@ close all
 clc
 
 L=pi;
-N=100;
+N=10;
 T=10;
 dx=L/(N+1);
 dt=T/(N+1);
 k=1;
 D=0.1;
-x=zeros(N,1);
-for ii=1:N
-    x(ii)=ii*dx;
+F=zeros(N,N+1);
+x=zeros(N+2,1);
+for ii=1:N+1
+    x(ii+1)=ii*dx;
 end
 
-t=zeros(N,1);
-for ii=1:N
-    t(ii)=ii*dt;
+t=zeros(N+2,1);
+for ii=1:N+1
+    t(ii+1)=ii*dt;
 end
 
-u=zeros(N,N+1);
-u_0_t=0;
-u_L_t=0;
+u=zeros(N+2);
 u(:,1)=sin(k*x);
 U=zeros(N);
-for ii=1:N
-    for jj=1:N
-        U(ii,jj)=exp(-D*k^2*jj*dt)*sin(k*ii*dx);
+for ii=1:N+2
+    for jj=1:N+2
+        U(ii,jj)=exp(-D*k^2*t(jj))*sin(k*x(ii));
     end
 end
 
 r=D*dt/dx^2;
 A_n=2*(1-r)*eye(N)+r*diag(ones(N-1,1),1)+r*diag(ones(N-1,1),-1);
-A_n_1=2*(1+r)*eye(N)-r*diag(ones(N-1,1),1)-r*diag(ones(N-1,1),-1);
-for jj=2:N+1
-    u(:,jj)=A_n_1\(A_n*u(:,jj-1));
+
+a=2*(1+r)*ones(N,1);
+b=-r*ones(N,1);
+c=b;
+for jj=2:N+2
+    f=A_n*u(2:N+1,jj-1)+F(:,jj-1);
+    f(1,1)=f(1,1)-u(1,jj);
+    f(N,1)=f(N,1)-u(N+2,jj);
+    u(2:N+1,jj)=tridiag(a,b,c,f);
 end
 
-u=u(:,2:N+1);
 
-e=abs(U-u)./abs(U);
+e=abs((U-u)./U);
 error=zeros(1,N);
 for ii=1:N
-    error(ii)=sum(e(:,ii))/N;
+    error(ii)=sum(e(2:N+1,ii+1))/N;
 end
-plot(t',error)
+plot(t(2:N+1)',error)
