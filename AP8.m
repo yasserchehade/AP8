9490c9cdb915e8fa2833bbd3503e8683d2c3c081
yasserchehade <36651977+yasserchehade@users.noreply.github.com>
2018-04-03 23:23:05 -0500

    Update AP8.m

--- AP8.m
+++ AP8.m
@@ -4,7 +4,7 @@ close all
 clc
 
 L=pi;
-N=1000;
+N=100;
 T=10;
 dx=L/(N+1);
 dt=T/(N+1);
@@ -42,19 +42,23 @@ for jj=2:N+2
     f(N,1)=f(N,1)+r*(u(N+2,jj)+u(N+2,jj-1));
     u(2:N+1,jj)=tridiag(a,b,c,f);
 end
-
-
-e=abs((U-u)./U);
-error=zeros(1,N);
-for ii=1:N
-    error(ii)=sum(e(2:N+1,ii+1))/N;
+%% graphing 1
+for a=[1/4 1/2 3/4]
+    j=round((N+1)*a);
+    figure
+    plot(x,u(:,j),x,U(:,j))
+    legend('calculated','exact')
+    title(['plot comaprison for t=' num2str(T*a) ' of calculated and exact' ])
+    xlabel('x')
+    ylabel('u(x,t)')
+  
 end
-plot(t(2:N+1)',error)
+
+
 %% 2
 
-
 k=1;
-w=0.001/(dt);
+w=0.1/(dt);
 
 x=zeros(N+2,1);
 for ii=1:N+1
@@ -68,7 +72,7 @@ end
 F=zeros(N+2);
 for ii=1:N+2
     for jj=1:N+2
-        F(jj,ii)=(w*cos(w*t(ii))+D*k^2*sin(w*t(ii)))*cos(k*x(jj));
+        F(ii,jj)=(w*cos(w*t(jj))+D*(k^2)*sin(w*t(jj)))*cos(k*x(ii));
     end
 end
 u=zeros(N+2);
@@ -87,17 +91,70 @@ A_n=2*(1-r)*eye(N)+r*diag(ones(N-1,1),1)+r*diag(ones(N-1,1),-1);
 a=2*(1+r)*ones(N,1);
 b=-r*ones(N,1);
 c=b;
+
 for jj=2:N+2
     f=A_n*u(2:N+1,jj-1)+2*dt*F(2:N+1,jj-1);
     f(1,1)=f(1,1)+r*(u(1,jj)+u(1,jj-1));
     f(N,1)=f(N,1)+r*(u(N+2,jj)+u(N+2,jj-1));
     u(2:N+1,jj)=tridiag(a,b,c,f);
+ 
+end
+%% graphing 2
+for a=[1/4 1/2 3/4]
+    j=round((N+1)*a);
+    figure
+    plot(x,u(:,j),x,U(:,j))
+    legend('calculated','exact')
+    title(['plot comaprison for t=' num2str(T*a) ' of calculated and exact' ])
+    xlabel('x')
+    ylabel('u(x,t)')
+  
 end
 
 
-e=abs((U-u)./U);
-error=zeros(1,N);
-for ii=1:N
-    error(ii)=sum(e(2:N+1,ii+1))/N;
+%%
+F=zeros(N+2);
+u=zeros(N+2);
+U=zeros(N);
+v=15;
+error=zeros(1,v);
+w=zeros(1,v);
+for z=1:v
+    w(z)=0.1*(z)/dt;
+
+    for ii=1:N+2
+        for jj=1:N+2
+            F(jj,ii)=(w(z)*cos(w(z)*t(ii))+D*k^2*sin(w(z)*t(ii)))*cos(k*x(jj));
+        end
+    end
+
+    u(1,:)=sin(w(z)*t);
+    u(N+2,:)=sin(w(z)*t)*cos(k*L);
+
+    for ii=1:N+2
+        for jj=1:N+2
+            U(ii,jj)=sin(w(z)*t(jj))*cos(k*x(ii));
+        end
+    end
+
+    r=D*dt/dx^2;
+    A_n=2*(1-r)*eye(N)+r*diag(ones(N-1,1),1)+r*diag(ones(N-1,1),-1);
+
+    a=2*(1+r)*ones(N,1);
+    b=-r*ones(N,1);
+    c=b;
+    for jj=2:N+2
+        f=A_n*u(2:N+1,jj-1)+2*dt*F(2:N+1,jj-1);
+        f(1,1)=f(1,1)+r*(u(1,jj)+u(1,jj-1));
+        f(N,1)=f(N,1)+r*(u(N+2,jj)+u(N+2,jj-1));
+        u(2:N+1,jj)=tridiag(a,b,c,f);
+    end
+
+
+    e=abs((U-u)./U);
+    error(z)=sum(e(2:N+1,N+2))/N;
 end
-plot(t(2:N+1)',error)
+plot(w,error)
+title(['error vs w'])
+xlabel('w')
+ylabel('error')
